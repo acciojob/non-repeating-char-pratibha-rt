a55bf5b338ae8ef6404d24001dfd29e7dcc912c9
pratibha-rt <pratibha.sept19@gmail.com>
2025-08-28 14:04:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let charCount = {};
+	for (let c of str) {
+		charCount[c] = (charCount[c] || 0) + 1;
+	}
+
+	for (let c of str) {
+		if (charCount[c] === 1) {
+			return c;
+		}
+	}
+	return null;
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
